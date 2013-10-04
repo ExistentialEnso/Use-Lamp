d0b34b2cba4868a7c2aa83a898c21941f1037092
Thorne Melcher <thornem@sylvane.com>
2013-10-04 00:38:20 -0400

    Break out client JS/CSS into assets folder.

--- client.php
+++ client.php
@@ -8,35 +8,12 @@
 <html>
   <head>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        $.post("/use-lamp/command.php", {"cmd":"load"}, function(data) {
-          $("#output_buffer").append(data);
-        });
-
-        $("#command_line").keydown(function(event) {
-          if(event.keyCode == 13) {
-            command = $("#command_line").val();
-
-            $.post("/use-lamp/command.php", {"cmd":command}, function(data) {
-              $("#output_buffer").append("<br />" + data);
-            });
-
-            $("#command_line").val("");
-
-            $("#output_buffer").append("<br /><br />>" + command);
-
-            // scroll to bottom of output buffer
-            var objDiv = document.getElementById("output_buffer");
-            objDiv.scrollTop = objDiv.scrollHeight;
-          }
-        });
-      });
-    </script>
+    <script src="assets/client.js" ></script>
+    <link rel="stylesheet" type="text/css" href="assets/client.css">
   </head>
   <body>
-  <div id="output_buffer" style="width:100%; height:100%; margin-bottom:10px; overflow:auto; font-family:monospace"></div>
+  <div id="output_buffer"></div>
   <br />
-  <input id="command_line" type="text" style="width:100%; font-family:monospace; position:absolute; bottom:0px" />
+  <input id="command_line" type="text" />
   </body>
 </html>
